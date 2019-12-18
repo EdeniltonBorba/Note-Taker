ae7eec2bc214912e9bccd82ee5d694e7e41ee84f
Edenilton <edeniltonborba.adm@gmail.com>
2019-12-18 12:34:45 +0100

    add apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,4 +11,39 @@ module.exports = function(app) {
             return res.json(error);
         }
     });
-}
+
+    app.post("/api/notes", async function(req, res) {
+        try {
+            const newNote = req.body || {};
+            let saveNotes = await fs.readFile("db/db.json", "utf-8");
+            let arrNotes = saveNotes.length > 0 ? JSON.parse(saveNotes) : [];
+            let notesId = [];
+            arrNotes.forEach(objNotes => {
+                if (!notesId.includes(objNotes.id) && objNotes.hasOwnProperty("id")) {
+                    notesId.push(objNotes.id);
+                }
+            });
+            newNote.id = notesId.length > 0 ? Math.max(...notesId) + 1 : 1;
+            arrNotes.push(newNote);
+            fs.writeFile("db/db.json", JSON.stringify(arrNotes), "utf8");
+            res.json(arrNotes);
+        } catch (error) {
+            console.log(error);
+            return res.json(error);
+        }
+    });
+
+    app.delete("/api/notes/:id", async function(req, res) {
+        try {
+            let noteId = req.params.id;
+            let savedNotes = await fs.readFile("db/db.json", "utf-8");
+            let arrNotes = savedNotes.length > 0 ? JSON.parse(savedNotes) : [];
+            let newArrNotes =  arrNotes.filter(objNotes => objNotes.id != noteId);
+            fs.writeFile("db/db.json", JSON.stringify(newArrNotes), "utf8");
+            res.json(newArrNotes);
+        } catch (error) {
+            console.log(error);
+            return res.json(error);
+        }
+    });
+};
